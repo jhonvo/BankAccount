dd6a08e034070da84aa19a6f4360e31814c29b22
jhonvo <jhon26victor@gmail.com>
2021-09-22 15:01:48 -0500

    Updating content

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from Account import Account
 from User import User
 
-michaelUser = User( "Michael", "Miller", 30 )
-alvaroUser = User( "Alvaro", "Sanchez", 35 )
+print( "***** Bank Account MENU *****\n1) Add an account\n2) Deposit to an account\n3) Withdraw from an account" )
+print( "4) See the information of all accounts")
+option = input( "Select an option: " )
 
-michaelAccount = Account( 12345, michaelUser )
-alvaroAccount = Account( 22222, alvaroUser )
+while( option == "1" or option == "2" or option == "3" or option == "4" ):
+    if( option == "1" ):
+        firstName = input( "Please give me your first name: ")
+        lastName = input( "Please give me your last name: ")
+        age = input( "Please give me your age: ")
+        newUser = User( firstName, lastName, int(age) )
+        accountNo = input( "Type you account No: ")
+        newAccount = Account( int(accountNo), newUser )
+    if( option == "4" ):
+        Account.printAllAccountsInfo()
+    print( "***** Bank Account MENU *****\n1) Add an account\n2) Deposit to an account\n3) Withdraw from an account" )
+    print( "4) See the information of all accounts")
+    option = input( "Select an option: " )    
 
-michaelAccount.deposit( 10000.0 )
-alvaroAccount.deposit( 1000.0 )
+print( "End of the program" )
+#michaelUser = User( "Michael", "Miller", 30 )
+#alvaroUser = User( "Alvaro", "Sanchez", 35 )
 
-michaelAccount.transfer( alvaroAccount, 2000.0 )
+#michaelAccount = Account( 12345, michaelUser )
+#alvaroAccount = Account( 22222, alvaroUser )
 
-Account.printAllAccountsInfo()
+#michaelAccount.deposit( 10000.0 )
+#alvaroAccount.deposit( 1000.0 )
+
+#michaelAccount.transfer( alvaroAccount, 2000.0 )
+
+#Account.printAllAccountsInfo()
 
 
 #michaelAccount.withdraw( 100.0 )
